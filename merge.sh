e6e7f4ae8d635fc781fb39c71a2141a69afe3ea9
p1k0pan <645758634@qq.com>
2025-05-16 12:21:43 +0200

    2025.5.16 merge

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
 swift export \
-    --ckpt_dir /mnt/workspace/xintong/pjh/All_result/zh_tox_lora/lora_weights_full_data/v0-20250516-170751/ \
+    --adapters /mnt/workspace/xintong/pjh/All_result/zh_tox_lora/lora_weights_full_data/v0-20250516-170751/checkpoint-387 \
     --merge_lora true \
     --output_dir /mnt/workspace/xintong/pjh/All_result/zh_tox_lora/merged_lora/qwen3-32b-tox-classifier/
